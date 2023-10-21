af411fdd9a8e28a5b15bbb8a9755e3e022a5be38
khalilcherni <kcherni411@gmail.com>
2023-10-21 10:25:29 +0100

    sprint2 done

--- main.js
+++ main.js
@@ -30,4 +30,76 @@
 
 // Write a function called hasUniqueCharacters that takes a string as input and returns true if all the characters in the string are unique (i.e., none of the characters repeat), and false otherwise.
 
+// Write a function called isPrimeProduct that takes two positive integers as input and returns true if their product is a prime number, and false otherwise.main.js
+
+
+
+function calculator(a,o,b) {
+    if (o === '+') {
+        return a + b
+  }else    if (o === '-') {
+        return a - b
+}else    if (o === '*') {
+        return a * b
+    }
+    else if (o==='/'){
+        return a/b
+    }
+     if (b===0){
+        return null
+
+    }
+
+    function basicCalculator(a, o, b) {
+     if (o === '+') {
+        return a + b;
+      } else if (o === '-') {
+         return a - b;
+        } else if (o === '*') {
+          return a* b;
+        } else if (o === '/') {
+          if (b === 0) {
+            return null; 
+          }
+          return a / b;
+        } else {
+          return null; 
+        }
+      }
+}
+2//
+function isLeapYear(x){
+  return  (x%4===0 && x%100 !==0) ||(x%400===0) 
+}
+3//
+function isValidPassword(password){
+    var uppercaseRegex = /[A-Z]/
+    var lowercaseRegex = /[a-z]/
+    var numberRegex = /\d/
+    if ((password.length>=8)&&(uppercaseRegex&&lowercaseRegex)&& numberRegex.test(password))
+    {return true }
+    else {
+        return false
+    }
+}
+
+
+
+// Write a function called hasUniqueCharacters that takes a string as input and returns true if all the characters in the string are unique (i.e., none of the characters repeat), and false otherwise.
+var format = /[!@#$%^&*()_+-=[]{};':"\|,.<>/?]+/
+function hasUniqueCharacters(chara){
+  if (format.test(chara)){
+    return "true"
+  }else{
+    return "false"
+  }
+}
 // Write a function called isPrimeProduct that takes two positive integers as input and returns true if their product is a prime number, and false otherwise.
+
+function primenums(num1,num2){
+  var prime=num1+num2
+  if (prime===2 && 3 && 5 && 7 && 11 && 13 && 17 && 19 && 23 && 29 && 31 && 37 && 41 && 43 && 47 && 53 && 59 && 61 && 67 && 71 && 73 && 79 && 83 && 89 && 97){
+    return "prime number"
+  }return "false"
+}
+
